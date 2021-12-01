3dfc5835637e3f81034f9e1c2646bc016fb1f1c1
Benoitrenou <benoit.renou@gmail.com>
2021-12-01 09:13:13 -0300

    Update
    Add class based on ModelAdmin

--- LITReview/reviews/admin.py
+++ LITReview/reviews/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import Ticket, Review, UserFollow
 
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'has_review', 'id')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('headline', 'user', 'ticket', 'id')
+
+class UserFollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'followed_user', 'id')
+
+
 # Register your models here.
-admin.site.register(Ticket)
-admin.site.register(Review)
-admin.site.register(UserFollow)
+admin.site.register(Ticket, TicketAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(UserFollow, UserFollowAdmin)
